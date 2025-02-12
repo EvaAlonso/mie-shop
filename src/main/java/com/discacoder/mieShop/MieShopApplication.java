577d2c8b41452fee4e39ab5f6fb806d3b066736d
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-02-12 10:52:41 +0100

    initial commit

---
+++ src/main/java/com/discacoder/mieShop/MieShopApplication.java
@@ -0,0 +1,13 @@
+package com.discacoder.mieShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MieShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MieShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discacoder/mieShop/MieShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discacoder.mieShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MieShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
