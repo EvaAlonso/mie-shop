382b8b8ba3d03a3c9e3a8af9820c0f5f5654f3ae
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-02-14 09:01:22 +0100

    Category service added

---
+++ src/main/java/com/discacoder/mieShop/exceptions/AlredyExistsException.java
@@ -0,0 +1,7 @@
+package com.discacoder.mieShop.exceptions;
+
+public class AlredyExistsException extends RuntimeException {
+    public AlredyExistsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/discacoder/mieShop/service/category/CategoryService.java
+++ src/main/java/com/discacoder/mieShop/service/category/CategoryService.java
@@ -1,4 +1,53 @@
 package com.discacoder.mieShop.service.category;
 
-public class CategoryService {
+import com.discacoder.mieShop.exceptions.AlredyExistsException;
+import com.discacoder.mieShop.exceptions.ResourceNotFoundException;
+import com.discacoder.mieShop.model.Category;
+import com.discacoder.mieShop.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService{
+    private final CategoryRepository categoryRepository;
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Category not found!"));
+    }
+
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category addCategory(Category category) {
+        return Optional.of(category).filter(c -> !categoryRepository.existsByName(c.getName()))
+                .map(categoryRepository::save)
+                .orElseThrow(()-> new AlredyExistsException(category.getName() + " already exists"));
+    }
+
+    public Category updateCategory(Category category, Long id) {
+        return Optional.ofNullable(getCategoryById(id))
+                .map(oldCategory -> {
+                    oldCategory.setName(category.getName());
+                    return categoryRepository.save(oldCategory);
+                }).orElseThrow(()->new ResourceNotFoundException("Category not found!"));
+    }
+
+
+
+    public void deleteCategoryById(Long id) {
+        categoryRepository.findById(id)
+                .ifPresentOrElse(categoryRepository::delete, ()->{
+                    throw new ResourceNotFoundException("Category not found!");
+                });
+    }
 }
